45658f9cb506a4a357845820f1341b2b8cfedbbb
Alan Hanafy <alan.hanafy@gmail.com>
2023-03-07 14:49:23 -0500

    Update variables.tf

--- simple-instance/variables.tf
+++ simple-instance/variables.tf
@@ -9,12 +9,15 @@ variable "name" {
 variable "plan" {
   type        = string
   description = "This is the plan"
+  default     = "lemur"
 }
 variable "region" {
   type        = string
   description = "This is the region"
+  default     = "amazon-web-services::us-west-1"
 }
 variable "tags" {
   type        = list(string)
   description = "These are the tags"
+  default     = []
 }
