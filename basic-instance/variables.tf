9fb199c78eda71f34a6980b9cc3f6b8a13ceea54
Alan Hanafy <alan.hanafy@gmail.com>
2023-03-09 14:28:45 -0500

    Update variables.tf

--- basic-instance/variables.tf
+++ basic-instance/variables.tf
@@ -1,3 +1,8 @@
+variable "apikey" {
+  type        = string
+  description = "This is the CloudAMQP APIKEY"
+  default = ""
+}
 variable "name" {
   type        = string
   description = "This is the name"
@@ -5,12 +10,15 @@ variable "name" {
 variable "plan" {
   type        = string
   description = "This is the plan"
+  default     = "lemur"
 }
 variable "region" {
   type        = string
   description = "This is the region"
+  default     = "amazon-web-services::us-west-1"
 }
 variable "tags" {
   type        = list(string)
   description = "These are the tags"
+  default     = []
 }
